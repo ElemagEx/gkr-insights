61bc85b3b66e754feb6082d0b7638ea437c31237
ElemagEx <dragomir.stoichev@gmail.com>
2023-11-19 13:11:48 +0200

    fixed sub-module version

--- main.cpp
+++ main.cpp
@@ -4,7 +4,18 @@
 
 int main()
 {
-    gkr::stamp_now();
+    auto stamp = gkr::stamp_now();
 
+    struct tm tm;
+    gkr::stamp_decompose(true, stamp, tm);
+
+    std::printf("Now is %02d:%02d:%02d - %02d/%02d/%04d\n",
+        tm.tm_hour,
+        tm.tm_min,
+        tm.tm_sec,
+        tm.tm_mday,
+        tm.tm_mon  + 1,
+        tm.tm_year + 1900
+        );
     return 0;
 }
